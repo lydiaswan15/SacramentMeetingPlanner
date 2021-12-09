026ee1db4fa529313428a5d0cceb5559a366a742
Cierra Morris <4cibe@MacBook-Pro-10.local>
2021-12-09 13:58:08 -0700

    added view data annotations

--- SacramentMeetingPlanner/Models/BishopricMember.cs
+++ SacramentMeetingPlanner/Models/BishopricMember.cs
@@ -1,9 +1,11 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SacramentMeeting.Models
 {
     public class BishopricMember
     {
+        [Display(Name = "Conducting")]
         public int BishopricMemberId { get; set; }
         public string BishopricMemberName { get; set; }
     }
--- SacramentMeetingPlanner/Models/SacramentPlan.cs
+++ SacramentMeetingPlanner/Models/SacramentPlan.cs
@@ -17,20 +17,23 @@ namespace SacramentMeeting.Models
         public DateTime Date { get; set; }
 
 
-        [Display(Name = "Conducting")]
         public int BishopricMemberId { get; set; }
+        [Display(Name = "Conducting")]
         public BishopricMember BishopricMember { get; set; }
         public int OpeningHymnId { get; set; }
         [Display(Name = "Opening Hymn")]
         public Hymn OpeningHymn { get; set; }
         [Display(Name = "Closing Hymn")]
         public int ClosingHymnId { get; set; }
+        [Display(Name = "Closing Hymn")]
         public Hymn ClosingHymn { get; set; }
         [Display(Name = "Sacrament Hymn")]
         public int SacramentHymnId { get; set; }
+        [Display(Name = "Sacrament Hymn")]
         public Hymn SacramentHymn { get; set; }
         [Display(Name = "Intermediate Hymn")]
         public int? IntermediateHymnId { get; set; }
+        [Display(Name = "Intermediate Hymn")]
         public Hymn IntermediateHymn { get; set; }
         [Display(Name = "Opening Prayer")]
         public string OpeningPrayer { get; set; }
